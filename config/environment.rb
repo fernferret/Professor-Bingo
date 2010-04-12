8cee530e52d4f8ecb93c1db15e0b6c04e3f7ed78
Eric Stokes <fernferret@gmail.com>
2010-04-12 15:27:00 -0400

    Again...

--- config/environment.rb
+++ config/environment.rb
@@ -38,7 +38,7 @@ Rails::Initializer.run do |config|
   # The default locale is :en and all translations from config/locales/*.rb,yml are auto loaded.
   # config.i18n.load_path += Dir[Rails.root.join('my', 'locales', '*.{rb,yml}')]
   # config.i18n.default_locale = :de
-  config.gem 'haml', :version => '2.2.3'
+  config.gem 'haml', :version => '2.2.23'
   config.gem 'authlogic'
 end
 
